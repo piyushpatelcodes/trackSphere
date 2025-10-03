181166626cd1cdb465d95818f6b80138369c9b6d
Piyush Patel <piyushpatelcodes@gmail.com>
2025-10-03 17:47:57 +0530

    uri updated

--- frontend/src/utils/appoloClient.ts
+++ frontend/src/utils/appoloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
+// link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
+  link: new HttpLink({ uri: 'https://track-sphere-backend.vercel.app/graphql' }),
   cache: new InMemoryCache(),
 });
 
